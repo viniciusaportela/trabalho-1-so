e9413bf59cb33d9b0ab9eb2ce5c084f6dbb6e6e8
Giulia <Giulia@MacBook-de-Giulia.local>
2022-10-29 18:57:25 -0300

    criado as telas

--- core/ui.py
+++ core/ui.py
@@ -1,20 +1,43 @@
+from curses import window
 import PySimpleGUI as sg
 
 
 class Ui:
-    def initialize(self, board):
+    def initialize(self):
+        layout = [[]]
+        for row in range(10):
+            row_layout = []
+            for col in range(10):
+                row_layout.append(sg.Button('', key = (row, col), size=(2,2)))
+            layout.append(row_layout)
+        layout += [[sg.Button('SAIR', font=70)]]
+        window = sg.Window('Trabalho I - S.O.', layout)
+    
+    def winning_screen(self):
         layout = [
-            sg.Graph(
-                canvas_size=(400, 400),
-                graph_bottom_left=(0, 0),
-                graph_top_right=(400, 400),
-                background_color='red', # DEV to white
-                enable_events=True,
-                key='graph'
-            )
+            [sg.Text("PARABÉNS, VOCÊ GANHOU!", font=("Helvica",35))],
+            [sg.Text("Deseja Jogar Novamente?", font=("Helvica",20))],
+            [sg.Button("SIM"), sg.Button("NÃO")],
         ]
+        window = sg.Window('Trabalho I - S.O.', layout, element_justification='c')
 
-        self.window = sg.Window('Trabalho I - S.O.', layout)
+    def loser_screen(self):
+        layout = [
+            [sg.Text("NÃO FOI DESSA VEZ!", font=("Helvica",35))],
+            [sg.Text("Deseja Jogar Novamente?", font=("Helvica",20))],
+            [sg.Button("SIM"), sg.Button("NÃO")],
+        ]
+        window = sg.Window('Trabalho I - S.O.', layout, element_justification='c')
+    
+    def level_screen(self):
+        layout = [
+            [sg.Text("ESCOLHA UMA OPÇÃO ", font=("Helvica",35))],
+            [sg.Text("Níveis de Dificuldade", font=("Helvica",20))],
+            [sg.Button("FÁCIL"), sg.Button("MÉDIO"), sg.Button("DIFÍCIL")],
+        ]
+        window = sg.Window('Trabalho I - S.O.', layout, element_justification='c')
 
     def refresh(self, board):
         pass
+
+
